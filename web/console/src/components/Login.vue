2de17c6cc6aa9576ed9122a7d5bbb4eb08cf59b2
kun <kun@moov.cc>
2017-09-05 16:59:27 +0800

    add Login.vue

---
+++ web/console/src/components/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div>
+      <img ref="../assets/img/head.gif"/>
+    </div>
+    <div>
+      <p>提示</p>
+      <p>现在为您服务的是设在北京的中央服务器</p>
+      <p>請您首先選擇產品</p>
+      <p>如果您的IP 已被授權，請直接登錄</p>
+      <p>如果您的IP未被授權，請輸入用戶名和密碼後登錄</p>
+      <p>有問題請聯繫管理員</p>
+    </div>
+    <div>
+      <el-row>
+        <el-radio-group v-model="librarySelection">
+          <el-radio label="ancient">古典</el-radio>
+          <el-radio label="recent">近代</el-radio>
+        </el-radio-group>
+      </el-row>
+      <el-row>
+        <el-input v-model="userName" placeholder="用户名" />
+      </el-row>
+      <el-row>
+        <el-input v-model="password" placeholder="密码" />
+      </el-row>
+      <el-row>
+        <el-button @click="login()">登录</el-button>
+      </el-row>
+      <el-row>
+        <el-col :span="3">
+          <el-checkbox v-model="shouldSave">保存?</el-checkbox>
+        </el-col>
+        <el-col :span="6">
+          <template v-if="!ipAvailable">
+            您的IP {{ myIP }} 未被授权!
+          </template>
+        </el-col>
+      </el-row>
+    </div>
+    <div>
+      <p>版權所有 Copyright © 2017</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    ipAvailable () {
+      return false
+    },
+    myIP () {
+      return '127.0.0.1'
+    }
+  },
+  data() {
+    return {
+      librarySelection: 'ancient',
+      userName: '',
+      password: '',
+      shouldSave: false,
+    }
+  }
+}
+</script>
+<style>
+
+</style>
--- web/console/src/components/Main.vue
+++ web/console/src/components/Main.vue
@@ -1,82 +1,30 @@
 <template>
   <div id="app">
     <el-tabs type="card" v-model="activeTab" @tab-click="tabSwitch" ref="tab">
-      <el-tab-pane label="Workouts" name="workout_list">
-        <WorkoutList />
-      </el-tab-pane>
-      <el-tab-pane label="Flags" name="flags_manager">
-        <FlagList />
-      </el-tab-pane>
-      <el-tab-pane label="Coaches" name="coach_manager">
-        <CoachList />
-      </el-tab-pane>
-      <el-tab-pane label="Movements" name="movement_library">
-        <MovementLibrary :loadVideosOnLoad="false" />
-      </el-tab-pane>
-      <el-tab-pane label="Text Templates" name="text_template_editor">
-        <TextTemplateEditor />
-      </el-tab-pane>
-      <el-tab-pane label="Dev Panel" name="dev_panel">
-        <DevPanel />
-      </el-tab-pane>
-      <el-tab-pane label="Re-Import Console" name="import_console">
-        <ImportConsole />
-      </el-tab-pane>
-      <el-tab-pane label="Jenkins Console" name="jenkins_console">
-        <JenkinsConsole />
-      </el-tab-pane>
-      <el-tab-pane label="BI Console" name="bi_console">
-        <BIConsole />
+      <el-tab-pane label="Login" name="login">
+        <Login />
       </el-tab-pane>
     </el-tabs>
-    <GSControl style="position: absolute; top: 8px; right: 8px;" />
   </div>
 </template>
 
 <script>
-import WorkoutList from './WorkoutList.vue'
-import FlagList from './FlagList.vue'
-import CoachList from './CoachList.vue'
-import MovementLibrary from './MovementLibrary.vue'
-import TextTemplateEditor from './TextTemplateEditor.vue'
-import DevPanel from './DevPanel.vue'
-import ImportConsole from './ImportConsole.vue'
-import GSControl from '@/common/components/GSControl.vue'
-import JenkinsConsole from './JenkinsConsole.vue'
-import BIConsole from './BIConsole.vue'
+import Login from './Login.vue'
 
 export default {
   name: 'main',
   components: {
-    WorkoutList,
-    FlagList,
-    CoachList,
-    MovementLibrary,
-    GSControl,
-    DevPanel,
-    ImportConsole,
-    TextTemplateEditor,
-    JenkinsConsole,
-    BIConsole,
+    Login
   },
   data () {
     return {
       isDevMode: !!document.location.search.match('dev=1'),
-      activeTab: this.$route.query.active_tab || 'workout_list',
+      activeTab: this.$route.query.active_tab || 'login',
     };
   },
   methods: {
-    setJenkinsError(err) {
-      this.isJenkinsError = err
-    },
     tabSwitch() {
-      this.$router.replace({ name: 'root', query: { active_tab: this.activeTab }})
-    },
-    handleJenkinsError(e) {
-      this.$notify.error({
-        title: 'Jenkins error',
-        message: 'Please go to Jenkins Console and resolve them'
-      })
+      this.$router.replace({ name: 'login', query: { active_tab: this.activeTab }})
     },
   },
 }
